9e8c7757f080268f85a97ff25538f03bad0e3611
楼教主 <cik520@qq.com>
2017-05-01 00:21:00 +0800

    Optimized output format

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+const util = require('util');
 const internal = require('./lib/internal');
 const external = require('./lib/external');
 
-internal(data => console.log('internal:', data));
-external(data => console.log('external:', data));
+function inspect(data) {
+  const str = util.inspect(data);
+  const nolf = str.replace(/[\s\r\n]+/g, ' ');
+  return nolf.length > 80 ? str : nolf;
+}
+
+internal(data => console.log('internal:', inspect(data)));
+external(data => console.log('external:', inspect(data)));
