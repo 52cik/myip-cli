435ec16c058470ed058e49387021bbbf823677a8
cat-kun <rent_calculator@126.com>
2023-07-26 12:29:04 +0800

    feat: 优化

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const internal = require('./lib/internal');
 const external = require('./lib/external');
 
-internal().then(res => console.log('internal:', res));
-external().then(res => console.log('external:', res));
+internal().then(res => console.log('本机IP:', res));
+external().then(res => console.log('外网IP:', res));
